ee259584c46bfe9802dd869082ba7d9357f6fb0f
mikeluxue <daotian1825@gmail.com>
2023-04-11 14:32:51 +0800

    feat(core): increase default tx expiration time

--- trident-java/core/src/main/java/org/tron/trident/core/ApiWrapper.java
+++ trident-java/core/src/main/java/org/tron/trident/core/ApiWrapper.java
@@ -105,7 +105,7 @@ import static org.tron.trident.proto.Response.TransactionReturn.response_code.SU
  */
 
 public class ApiWrapper {
-    public static final long TRANSACTION_DEFAULT_EXPIRATION_TIME = 60 * 1_000L; //60 seconds
+    public static final long TRANSACTION_DEFAULT_EXPIRATION_TIME = 300 * 1_000L; //300 seconds
 
     public final WalletGrpc.WalletBlockingStub blockingStub;
     public final WalletSolidityGrpc.WalletSolidityBlockingStub blockingStubSolidity;
