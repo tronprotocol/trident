21486a7cb5e7165d87a938ea6e9b2d2d71dad655
0xbigapple <apple0xlee@gmail.com>
2025-03-07 14:00:10 +0800

    add docs

--- trident-java/core/src/main/java/org/tron/trident/core/exceptions/ContractCreateException.java
+++
@@ -1,8 +0,0 @@
-package org.tron.trident.core.exceptions;
-
-public class ContractCreateException extends Exception {
-
-  public ContractCreateException(String message) {
-    super(message);
-  }
-}
